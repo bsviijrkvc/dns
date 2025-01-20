f034e99e45cbca7a56a8c72b060afbe54515c562
bsviijrkvc <bsviijrkvc@gmail.com>
2025-01-20 20:16:17 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,2 @@
-pkg upgrade -y
-pkg install openssh -y
-cd /data/data/com.termux/files/usr/etc/ssh/
-rm sshd_config
-wget https://raw.githubusercontent.com/bsviijrkvc/dns/refs/heads/main/sshd_config
+pkg upgrade
+pkg install openssh
